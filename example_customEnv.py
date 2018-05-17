c5d2c047813250e414b5c2290655f320579e4021
Antti Virtanen <you@example.com>
2018-05-17 16:25:17 +0300

    example custom ENV file

---
+++ example_customEnv.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+#copy and rename this file to customEnv.py 
+bot_url = "/bot012345678:AABBCCDDEEGG1234_ABC/"
+ical_url = "https://ics.teamup.com/feed/abcdefghijk/000000.ics"
